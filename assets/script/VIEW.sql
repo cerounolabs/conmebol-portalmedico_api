338e31e24591ee5d3e19571b95b82ad259d4efba
Christian Zelaya <christian@cerouno.com.py>
2019-10-15 12:40:07 -0300

    Fix LOGIN

--- assets/script/VIEW.sql
+++ assets/script/VIEW.sql
@@ -17,6 +17,7 @@ AS
     a.superiorCompetitionFifaId AS COMPETICION_PADRE_ID,
     a.status AS COMPETICION_ESTADO,
     a.season AS COMPETICION_ANHO,
+    b.matchFifaId AS JUEGO_CODIGO,
     b.matchDayDesc AS JUEGO_NOMBRE,
     b.status AS JUEGO_ESTADO,
     b.dateTimeLocal AS JUEGO_HORARIO,
@@ -31,9 +32,9 @@ AS
 
     FROM [comet].[competitions] a
     LEFT JOIN [comet].[matches] b ON a.competitionFifaId = b.competitionFifaId
-    LEFT JOIN [comet].[matches_teams] c1 ON b.matchFifaId = c1.matchFifaId AND c1.teamNature = 'AWAY'
+    LEFT JOIN [comet].[matches_teams] c1 ON b.matchFifaId = c1.matchFifaId AND c1.teamNature = 'HOME'
     LEFT JOIN [comet].[teams] d1 ON c1.teamFifaId = d1.teamFifaId
-    LEFT JOIN [comet].[matches_teams] c2 ON b.matchFifaId = c2.matchFifaId AND c2.teamNature = 'HOME'
+    LEFT JOIN [comet].[matches_teams] c2 ON b.matchFifaId = c2.matchFifaId AND c2.teamNature = 'AWAY'
     LEFT JOIN [comet].[teams] d2 ON c2.teamFifaId = d2.teamFifaId
     LEFT JOIN [comet].[matches_phases] e1 ON b.matchFifaId = e1.matchFifaId AND e1.phase = 'FIRST_HALF'
     LEFT JOIN [comet].[matches_phases] e2 ON b.matchFifaId = e2.matchFifaId AND e2.phase = 'SECOND_HALF'
