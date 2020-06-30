f846541f13fb086681ef70205ab1d4f02a000973
Christian Zelaya MACBOOK <christian@cerouno.com.py>
2020-06-30 16:07:51 -0400

    Ajuste 202006301607

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 	session_start();
 
-//	header("Access-Control-Allow-Origin: http://portalmedico.conmebol.com");
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	header("Access-Control-Allow-Origin: http://portalmedico.conmebol.com");
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Pragma, Authorization, Accept-Encoding");
 	header("Access-Control-Allow-Credentials: true");
 	header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+	header("Content-Type: application/json");
 
 	date_default_timezone_set('America/Asuncion');
 
@@ -17,6 +17,26 @@
 
 	$app = new \Slim\App($settings);
 	require __DIR__.'/../src/dependencies.php';
+
+	$app->add(new Tuupola\Middleware\HttpBasicAuthentication([
+		"secure"=> false,
+		"users" => [
+			"user_sfholox" => "ns3r_5fh0!0x"
+		],
+		"error" => function($response, $args) {
+			header("Content-Type: application/json; charset=utf-8");
+
+			$data			= [];
+			$data['code']	= 401;
+			$data['status'] = 'failure';
+			$data['message']= 'Error NO AUTORIZADO';
+	
+			$body = $response->getBody();
+			$body->write(json_encode($data, JSON_UNESCAPED_SLASHES));
+	
+			return $response->withBody($body);
+		}
+	]));
 
 	//ROUTES
 	require __DIR__.'/../src/routes.php';
