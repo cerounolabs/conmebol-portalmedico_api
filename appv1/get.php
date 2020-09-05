b82387e97cbabf32922326e42fb7ff609fb21d36
Christian Zelaya MACBOOK <christian@cerouno.com.py>
2020-09-04 20:48:48 -0400

    Ajuste 202009042048

--- appv1/get.php
+++ appv1/get.php
@@ -7732,6 +7732,165 @@
         return $json;
     });
 
+    $app->get('/v1/400/medico/usuario', function($request) {
+        require __DIR__.'/../src/connect.php';
+
+        $sql00  = "SELECT
+            a.PERFICCOD                         AS          persona_codigo,
+            a.PERFICNOM                         AS          persona_nombre,
+            a.PERFICUSE                         AS          persona_user,
+            a.PERFICCON                         AS          persona_contrasenha,
+            a.PERFICPAT                         AS          persona_path,
+            a.PERFICMAI                         AS          persona_email,
+            a.PERFICTEF                         AS          persona_telefono,
+            a.PERFICOBS                         AS          persona_observacion,
+            a.PERFICAUS                         AS          persona_usuario,
+            a.PERFICAFH                         AS          persona_fecha_hora,
+            a.PERFICAIP                         AS          persona_ip,
+
+            b.DOMFICCOD                         AS          tipo_estado_codigo,
+            b.DOMFICNOI                         AS          tipo_estado_nombre_ingles,
+            b.DOMFICNOC                         AS          tipo_estado_nombre_castellano,
+            b.DOMFICNOP                         AS          tipo_estado_nombre_portugues,
+
+            c.DOMFICCOD                         AS          tipo_acceso_codigo,
+            c.DOMFICNOI                         AS          tipo_acceso_nombre_ingles,
+            c.DOMFICNOC                         AS          tipo_acceso_nombre_castellano,
+            c.DOMFICNOP                         AS          tipo_acceso_nombre_portugues,
+
+            d.DOMFICCOD                         AS          tipo_perfil_codigo,
+            d.DOMFICNOI                         AS          tipo_perfil_nombre_ingles,
+            d.DOMFICNOC                         AS          tipo_perfil_nombre_castellano,
+            d.DOMFICNOP                         AS          tipo_perfil_nombre_portugues,
+
+            e.teamFifaId                        AS          equipo_codigo,
+            e.internationalShortName            AS          equipo_nombre,
+
+            f.DOMFICCOD                         AS          tipo_categoria_codigo,
+            f.DOMFICNOI                         AS          tipo_categoria_nombre_ingles,
+            f.DOMFICNOC                         AS          tipo_categoria_nombre_castellano,
+            f.DOMFICNOP                         AS          tipo_categoria_nombre_portugues
+            
+            FROM [adm].[PERFIC] a
+            INNER JOIN [adm].[DOMFIC] b ON a.PERFICEST = b.DOMFICCOD
+            INNER JOIN [adm].[DOMFIC] c ON a.PERFICTIP = c.DOMFICCOD
+            INNER JOIN [adm].[DOMFIC] d ON a.PERFICROL = d.DOMFICCOD
+            INNER JOIN [comet].[teams] e ON a.PERFICEQU = e.teamFifaId
+            INNER JOIN [adm].[DOMFIC] f ON a.PERFICCAT = f.DOMFICCOD
+
+            ORDER BY a.PERFICNOM";
+
+        try {
+            $connMSSQL  = getConnectionMSSQLv1();
+            $stmtMSSQL  = $connMSSQL->prepare($sql00);
+            $stmtMSSQL->execute();
+
+            while ($rowMSSQL = $stmtMSSQL->fetch()) {    
+                $persona_fecha_hora = date_format(date_create($rowMSSQL['persona_fecha_hora']), 'd/m/Y H:i:s');
+                
+                if (isset($rowMSSQL['persona_path'])){
+                    $persona_path = $rowMSSQL['persona_path'];
+                } else {
+                    $persona_path = 'assets/images/users/defaul.png';
+                }
+
+                $detalle    = array(
+                    'persona_codigo'                        => $rowMSSQL['persona_codigo'],
+                    'persona_nombre'                        => trim(strtoupper(strtolower($rowMSSQL['persona_nombre']))),
+                    'persona_user'                          => trim(strtoupper(strtolower($rowMSSQL['persona_user']))),
+                    'persona_contrasenha'                   => trim(strtoupper(strtolower($rowMSSQL['persona_contrasenha']))),
+                    'persona_path'                          => $persona_path,
+                    'persona_email'                         => trim(strtoupper(strtolower($rowMSSQL['persona_email']))),
+                    'persona_telefono'                      => trim(strtoupper(strtolower($rowMSSQL['persona_telefono']))),
+                    'persona_observacion'                   => trim(strtoupper(strtolower($rowMSSQL['persona_observacion']))),
+                    'persona_usuario'                       => trim(strtoupper(strtolower($rowMSSQL['persona_usuario']))),
+                    'persona_fecha_hora'                    => $persona_fecha_hora,
+                    'persona_ip'                            => trim(strtoupper(strtolower($rowMSSQL['persona_ip']))),
+
+                    'tipo_estado_codigo'                    => $rowMSSQL['tipo_estado_codigo'],
+                    'tipo_estado_nombre_ingles'             => trim(strtoupper(strtolower($rowMSSQL['tipo_estado_nombre_ingles']))),
+                    'tipo_estado_nombre_castellano'         => trim(strtoupper(strtolower($rowMSSQL['tipo_estado_nombre_castellano']))),
+                    'tipo_estado_nombre_portugues'          => trim(strtoupper(strtolower($rowMSSQL['tipo_estado_nombre_portugues']))),
+
+                    'tipo_acceso_codigo'                    => $rowMSSQL['tipo_acceso_codigo'],
+                    'tipo_acceso_nombre_ingles'             => trim(strtoupper(strtolower($rowMSSQL['tipo_acceso_nombre_ingles']))),
+                    'tipo_acceso_nombre_castellano'         => trim(strtoupper(strtolower($rowMSSQL['tipo_acceso_nombre_castellano']))),
+                    'tipo_acceso_nombre_portugues'          => trim(strtoupper(strtolower($rowMSSQL['tipo_acceso_nombre_portugues']))),
+
+                    'tipo_perfil_codigo'                    => $rowMSSQL['tipo_perfil_codigo'],
+                    'tipo_perfil_nombre_ingles'             => trim(strtoupper(strtolower($rowMSSQL['tipo_perfil_nombre_ingles']))),
+                    'tipo_perfil_nombre_castellano'         => trim(strtoupper(strtolower($rowMSSQL['tipo_perfil_nombre_castellano']))),
+                    'tipo_perfil_nombre_portugues'          => trim(strtoupper(strtolower($rowMSSQL['tipo_perfil_nombre_portugues']))),
+
+                    'equipo_codigo'                         => $rowMSSQL['equipo_codigo'],
+                    'equipo_nombre'                         => trim(strtoupper(strtolower($rowMSSQL['equipo_nombre']))),
+
+                    'tipo_categoria_codigo'                 => $rowMSSQL['tipo_categoria_codigo'],
+                    'tipo_categoria_nombre_ingles'          => trim(strtoupper(strtolower($rowMSSQL['tipo_categoria_nombre_ingles']))),
+                    'tipo_categoria_nombre_castellano'      => trim(strtoupper(strtolower($rowMSSQL['tipo_categoria_nombre_castellano']))),
+                    'tipo_categoria_nombre_portugues'       => trim(strtoupper(strtolower($rowMSSQL['tipo_categoria_nombre_portugues'])))
+                );
+
+                $result[]   = $detalle;
+            }
+
+            if (isset($result)){
+                header("Content-Type: application/json; charset=utf-8");
+                $json = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success SELECT', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            } else {
+                $detalle    = array(
+                    'persona_codigo'                        => '',
+                    'persona_nombre'                        => '',
+                    'persona_user'                          => '',
+                    'persona_contrasenha'                   => '',
+                    'persona_path'                          => '',
+                    'persona_email'                         => '',
+                    'persona_telefono'                      => '',
+                    'persona_observacion'                   => '',
+                    'persona_usuario'                       => '',
+                    'persona_fecha_hora'                    => '',
+                    'persona_ip'                            => '',
+
+                    'tipo_estado_codigo'                    => '',
+                    'tipo_estado_nombre_ingles'             => '',
+                    'tipo_estado_nombre_castellano'         => '',
+                    'tipo_estado_nombre_portugues'          => '',
+
+                    'tipo_acceso_codigo'                    => '',
+                    'tipo_acceso_nombre_ingles'             => '',
+                    'tipo_acceso_nombre_castellano'         => '',
+                    'tipo_acceso_nombre_portugues'          => '',
+
+                    'tipo_perfil_codigo'                    => '',
+                    'tipo_perfil_nombre_ingles'             => '',
+                    'tipo_perfil_nombre_castellano'         => '',
+                    'tipo_perfil_nombre_portugues'          => '',
+
+                    'equipo_codigo'                         => '',
+                    'equipo_nombre'                         => '',
+
+                    'tipo_categoria_codigo'                 => '',
+                    'tipo_categoria_nombre_ingles'          => '',
+                    'tipo_categoria_nombre_castellano'      => '',
+                    'tipo_categoria_nombre_portugues'       => ''
+                );
+
+                header("Content-Type: application/json; charset=utf-8");
+                $json = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => $detalle), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            }
+
+            $stmtMSSQL->closeCursor();
+            $stmtMSSQL = null;
+        } catch (PDOException $e) {
+            header("Content-Type: application/json; charset=utf-8");
+            $json = json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error SELECT: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+
+        $connMSSQL  = null;
+        
+        return $json;
+    });
+
     $app->get('/v1/801/examen/prueba/{equipo}/{encuentro}', function($request) {
         require __DIR__.'/../src/connect.php';
 
