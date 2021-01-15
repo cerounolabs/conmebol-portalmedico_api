b0ff6d265101ee7e600788ae3dec6e6e34960bd1
Maria Del Carmen Gaona <mgaona@cerouno.com.py>
2021-01-15 16:06:31 -0300

    Feature: agregue nuevos parametros

--- appv1/get.php
+++ appv1/get.php
@@ -11215,7 +11215,7 @@
                 } else {
                     $stmtMSSQL00->execute([$val02, $val04, $val01, $val03]);
                     $stmtMSSQL01->execute([$val03, $val01, $val02, $val02, $val04]);
-                    $stmtMSSQL02->execute([$val02, $val04, $val01, $val03]);
+                    $stmtMSSQL02->execute([$val02, $val04, $val01, $val03, $val02, $val04, $val01, $val03, $val02, $val04, $val01, $val03]);
                 }
 
                 $cantRegistro = 0;
