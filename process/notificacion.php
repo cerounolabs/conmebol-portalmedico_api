fa344e017733301d8da36b791af2988a4f4161ec
Maria Del Carmen Gaona <mgaona@cerouno.com.py>
2021-02-08 16:49:27 -0300

    feature: mensajes para controlar los recorridos

--- process/notificacion.php
+++ process/notificacion.php
@@ -149,7 +149,7 @@
 
                 $NOTFICCOD  = $rowMSSQL00['notificacion_codigo'];
                 $NOTFICCOC  = $rowMSSQL00['competicion_codigo'];
-               // $MENSAJE    = $rowMSSQL00(trim(['notificacion_titulo']));//.' '.$rowMSSQL00(trim(['notificacion_descripcion']));
+                $MENSAJE    = trim($rowMSSQL00['notificacion_titulo']).' '.trim($rowMSSQL00['notificacion_descripcion']);
                 
                 $stmtMSSQL01->execute([$NOTFICCOD]);
 
